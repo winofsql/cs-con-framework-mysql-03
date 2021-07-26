594278faf90c21629f20bf2fbabe1be5e1e1cdcf
sworc <lightbox@nifty.com>
2021-07-26 20:09:29 +0900

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Odbc;
+using System.Diagnostics;
+
+namespace cs_con_framework_mysql_03
+{
+    class Helper
+    {
+        static public OdbcConnection CreateConnection()
+        {
+            // 接続文字列の作成
+            OdbcConnectionStringBuilder builder = new OdbcConnectionStringBuilder();
+            builder.Driver = "MySQL ODBC 8.0 Unicode Driver";
+            // 接続用のパラメータを追加
+            builder.Add("server", "localhost");
+            builder.Add("database", "lightbox");
+            builder.Add("uid", "root");
+            builder.Add("pwd", "");
+
+            Console.WriteLine(builder.ConnectionString);
+            Debug.WriteLine($"Debug:{builder.ConnectionString}");
+
+            // 接続の作成
+            OdbcConnection myCon = new OdbcConnection();
+
+            // MySQL の接続準備完了
+            myCon.ConnectionString = builder.ConnectionString;
+
+            // MySQL に接続
+            myCon.Open();
+
+            return myCon;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Data.Odbc;
+
+namespace cs_con_framework_mysql_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSet dataSet = new DataSet();
+
+            OdbcConnection myCon = Helper.CreateConnection();
+
+            // MySQL の処理
+
+            // SQL
+            string myQuery =
+                @"SELECT * from 社員マスタ order by 社員コード";
+
+            // アタプター作成
+            OdbcDataAdapter adapter = new OdbcDataAdapter();
+
+            // SQL 作成
+            OdbcCommand command = new OdbcCommand(myQuery, myCon);
+            command.CommandType = CommandType.Text;
+            adapter.SelectCommand = command;
+
+            // 後で更新用コマンドを取り出す為に必要
+            OdbcCommandBuilder commandBuilder = new OdbcCommandBuilder(adapter);
+
+            // データを取り出す
+            adapter.Fill(dataSet);
+
+            // テーブルを取得
+            DataTable dataTable = dataSet.Tables["Table"];
+
+            foreach(DataRow row in dataTable.Rows)
+            {
+                int kyuyo = row.Field<int>("給与");
+                kyuyo++;
+                row["給与"] = kyuyo;
+                kyuyo = (int)row["給与"];
+                kyuyo++;
+                row["給与"] = kyuyo;
+
+                DateTime dt = (DateTime)row["生年月日"];
+                // 1日以前に変更
+                dt = dt.AddDays(1);
+                // 日付のセット
+                row["生年月日"] = dt;
+            }
+
+            // 更新用のコマンドを取得
+            adapter.UpdateCommand = commandBuilder.GetUpdateCommand();
+            // 更新実行
+            adapter.Update(dataSet);
+
+            myCon.Close();
+        }
+    }
+}
